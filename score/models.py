78b2545369380b56d0696cb93e1b885485f88a3b
Tony Teaches Tech <tony@tonyteaches.tech>
2023-02-20 05:46:04 +0000

    Define a model for scores

--- score/models.py
+++ score/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Score(models.Model):
+    name = models.CharField(max_length=50)
+    value = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.name
