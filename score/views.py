160a042037d6c4dd1e12b33962519e8393f9ed70
Tony Teaches Tech <tony@tonyteaches.tech>
2023-02-20 07:17:59 +0000

    Display table of scores

--- score/views.py
+++ score/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from score.models import Score
 
 def index(request):
-    return render(request, 'index.html')
+    context = {}
+    scores = Score.objects.all()
+    context['scores'] = scores
+    context['title'] = 'Home'
+    return render(request, 'index.html', context)
 
 def about(request):
-    return render(request, 'about.html')
+    context = {}
+    context['title'] = 'About'
+    return render(request, 'about.html', context)
