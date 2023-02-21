0b3027c8e9b68ffc7ee7e84b5a4c2aba9164fbe4
Tony Teaches Tech <tony@tonyteaches.tech>
2023-02-21 00:56:33 +0000

    Django CRUD implementation

--- score/views.py
+++ score/views.py
@@ -8,6 +8,24 @@ def index(request):
     scores = Score.objects.all()
     context['scores'] = scores
     context['title'] = 'Home'
+    if request.method == 'POST':
+        if 'save' in request.POST:
+            pk = request.POST.get('save')
+            if not pk:
+                form = ScoreForm(request.POST)
+            else:
+                score = Score.objects.get(id=pk)
+                form = ScoreForm(request.POST, instance=score)
+            form.save()
+            form = ScoreForm()
+        elif 'delete' in request.POST:
+            pk = request.POST.get('delete')
+            score = Score.objects.get(id=pk)
+            score.delete()
+        elif 'edit' in request.POST:
+            pk = request.POST.get('edit')
+            score = Score.objects.get(id=pk)
+            form = ScoreForm(instance=score)
     context['form'] = form
     return render(request, 'index.html', context)
 
